a961fb5cdf8859393d844ac0007f935bd5b1d3d4
Kali R <pascal.reither@googlemail.com>
2024-12-16 20:22:17 +0100

    add day 16 solutions

---
+++ 2024/16_1.py
@@ -0,0 +1,80 @@
+from enum import Enum
+
+def check_field(map, already_checked, position, field_to_check):
+    max_height = len(map) - 1
+    max_width = len(map[0]) - 2
+
+    if field_to_check[0] < 0 or field_to_check[0] > max_height or field_to_check[1] < 0 or field_to_check[1] > max_width:
+        return False
+    
+    if field_to_check in already_checked:
+        return False 
+    
+    if map[field_to_check[0]][field_to_check[1]] == map[position[0]][position[1]]:
+        return True
+    
+    return False
+
+class Direction(Enum):
+    RIGHT = 0
+    LEFT = 1
+    DOWN = 2
+    UP = 3
+
+class Node():
+    dist = -1
+    prev = -1
+    
+
+with open('input.txt') as f:
+    contents = f.readlines()
+
+
+directions = [(0, 1),(0, -1),(1, 0),(-1, 0)]
+unvisited_nodes = {}
+target = ()
+
+
+cnt = 0
+for y in range(len(contents)):
+    for x in range(len(contents[0][:-1])):
+        if contents[y][x] == "." or contents[y][x] == "E":
+            for d in Direction:
+                unvisited_nodes[(y,x,d)] = Node()
+            if contents[y][x] == "E":
+                target = (y, x)
+        elif contents[y][x] == "S":
+            unvisited_nodes[(y,x,Direction.RIGHT)] = Node()
+            unvisited_nodes[(y,x,Direction.RIGHT)].dist = 0
+
+while len(unvisited_nodes) > 0:
+    #search lowest dist
+    lowest_node = ()
+    lowest_val = -1
+    for key in unvisited_nodes:
+        if unvisited_nodes[key].dist >= 0:
+            if unvisited_nodes[key].dist < lowest_val or lowest_val == -1:
+                lowest_node = key
+                lowest_val = unvisited_nodes[key].dist
+
+    if lowest_val == -1:
+        break
+
+    if lowest_node[0] == target[0] and lowest_node[1] == target[1]:
+        print(unvisited_nodes[lowest_node].dist)
+        break
+
+    for d in Direction:
+        new_pos = (lowest_node[0] + directions[d.value][0], lowest_node[1] + directions[d.value][1], d)
+        if new_pos in unvisited_nodes:
+            edge_cost = 1
+            if lowest_node[2] != d:
+                edge_cost = 1001
+            new_distance = unvisited_nodes[lowest_node].dist + edge_cost
+
+            if new_distance < unvisited_nodes[new_pos].dist or unvisited_nodes[new_pos].dist == -1:
+                unvisited_nodes[new_pos].dist = new_distance
+                unvisited_nodes[new_pos].prev = lowest_node
+
+    del unvisited_nodes[lowest_node]
+
